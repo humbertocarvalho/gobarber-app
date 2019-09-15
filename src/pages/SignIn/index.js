21f357359fb83f8301c46942d7db4729e835ba6c
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-15 19:36:23 -0300

    Created Button component.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -3,6 +3,7 @@ import { Text } from 'react-native';
 
 import Background from '~/components/Background';
 import Input from '~/components/Input';
+import Button from '~/components/Button';
 // import { Container } from './styles';
 
 export default function SignIn() {
@@ -14,6 +15,7 @@ export default function SignIn() {
         icon="call"
         placeholder="Digite seu nome"
       />
+      <Button>Entrar</Button>
     </Background>
   );
 }
