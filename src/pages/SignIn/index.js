9dcd13f9af5572202ae5be78e3aa69866747adc7
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-15 19:29:38 -0300

    Created Input Component. Add other dependencies.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
 // import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
     </Background>
   );
 }
