2a0fcaf5206bd6c00301c2ee85b561ebb4f627b0
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-17 18:20:22 -0300

    Add style to the bottomTabNavigator.

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,26 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 export default (isSigned = false) =>
   createAppContainer(
     createSwitchNavigator(
       {
         Sign: createSwitchNavigator({ SignIn, SignUp }),
-        App: createBottomTabNavigator({ Dashboard }),
+        App: createBottomTabNavigator(
+          { Dashboard, Profile },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255,255,255,0.6)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          },
+        ),
       },
       {
         initialRouteName: isSigned ? 'App' : 'Sign',
