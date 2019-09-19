3a484166446949f37009f7f4bb8bf243cfcbe3f4
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-18 22:44:58 -0300

    Add isFocused.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { withNavigationFocus } from 'react-navigation';
 
 import Background from '~/components/Background';
-
 import Appointment from '~/components/Appointment';
 import { Container, Title, List } from './styles';
 import api from '~/services/api';
 
-export default function Dashboard() {
+function Dashboard({ isFocused }) {
   const [appointments, setAppointments] = useState([]);
 
-  useEffect(() => {
-    async function loadAppointments() {
-      const response = await api.get('appointments');
-      setAppointments(response.data);
-    }
+  async function loadAppointments() {
+    const response = await api.get('appointments');
+    setAppointments(response.data);
+  }
 
-    loadAppointments();
-  }, [setAppointments]);
+  useEffect(() => {
+    if (isFocused) {
+      loadAppointments();
+    }
+  }, [isFocused]);
 
   async function handleCancel(id) {
     const response = await api.delete(`appointments/${id}`);
@@ -50,3 +52,5 @@ Dashboard.navigationOptions = {
     <Icon name="event" size={20} color={tintColor} />
   ),
 };
+
+export default withNavigationFocus(Dashboard);
